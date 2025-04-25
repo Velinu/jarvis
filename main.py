d8676a815ece9a8a0e11a8924c87e35f84d04695
mfelinto <mfelinto@fatecie.edu.br>
2025-04-25 00:18:02 -0300

    feat: start speech recognition

---
+++ main.py
@@ -0,0 +1,33 @@
+# importing libraries
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# create a speech recognition object
+r = sr.Recognizer()
+mic = sr.Microphone(device_index=2)
+
+def ouvir_microfone():
+    #Habilita o microfone para ouvir o usuario
+    microfone = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        #Chama a funcao de reducao de ruido disponivel na speech_recognition
+        microfone.adjust_for_ambient_noise(source)
+        #Avisa ao usuario que esta pronto para ouvir
+        print("Diga alguma coisa: ")
+        #Armazena a informacao de audio na variavel
+        audio = microfone.listen(source)
+
+    try:
+        #Passa o audio para o reconhecedor de padroes do speech_recognition
+        frase = microfone.recognize_google(audio,language='pt-BR')
+        #Após alguns segundos, retorna a frase falada
+        print("Você disse: " + frase)
+        #Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+    except sr.UnkownValueError:
+        print("Não entendi")
+    return frase
+
+ouvir_microfone()
