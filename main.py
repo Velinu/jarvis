15edf9f4d6b8c948a4de28c26b62d88cea68369b
mfelinto <mfelinto@fatecie.edu.br>
2025-04-27 21:41:58 -0300

    start excute command

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # importing libraries
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 import speech_recognition as sr
 import os
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+import webbrowser
+import pyautogui
 
 # create a speech recognition object
+model = OllamaLLM(model='gemma3')
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=2)
+
+template = """
+Você é um assistente virtual chamado jarvis e tem a tarefa de identificar ações
+que o usuário deseja fazer e transcrever para alguma das possíbilidades
+
+caso o usuário deseje abrir o navegador retorne 1
+caso o usuário deseje abrir o bloco de notas retorne 2
+"""
 
 def ouvir_microfone():
     #Habilita o microfone para ouvir o usuario
@@ -25,9 +36,22 @@ def ouvir_microfone():
         frase = microfone.recognize_google(audio,language='pt-BR')
         #Após alguns segundos, retorna a frase falada
         print("Você disse: " + frase)
+        return frase.lower()
         #Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
     except sr.UnkownValueError:
         print("Não entendi")
     return frase
 
-ouvir_microfone()
+def executar_comando(comando):
+    if "abrir navegador" in comando:
+        webbrowser.open('https://www.google.com')
+    elif "abrir notepad" in comando:
+        os.system('notepad.exe')
+    elif "digitar teste" in comando:
+        pyautogui.write("Este é um teste de automação por voz!", interval=0.1)
+    else:
+        print("Comando não reconhecido.")
+
+c = ouvir_microfone()
+executar_comando(c)
+
